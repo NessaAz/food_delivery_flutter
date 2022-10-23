ddd263142bedb2913b47abf14e1b716abe7ddaad
NessaAz <vanessa.azenwa@gmail.com>
2022-10-23 12:13:06 +0300

    add utils file for Colors class

---
+++ lib/utils/colors.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+class AppColors {
+  static final Color textColor = const Color(0x283618);
+  static final Color mainColor = const Color(0xDDA15E);
+  static final Color iconColor = const Color(0xBC6C25);
+  static final Color iconColor2 = const Color(0xBC6C25);
+  static final Color paraColor = const Color(0xFCF09F);
+  static final Color buttonBackgroundColor = const Color(0x606C38);
+  static final Color signColor = const Color(0xBC6C25);
+  static final Color textColor = const Color(0x283618);
+  static final Color textColor = const Color(0x283618);
+  static final Color textColor = const Color(0x283618);
+
+}
